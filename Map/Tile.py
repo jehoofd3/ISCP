0e73832f996d0d662da73ade91f86d2eb11aac53
jeroenvo1 <jeroen_van_ottelen@hotmail.com>
2015-05-26 20:23:05 +0200

    Map

---
+++ Map/Tile.py
@@ -0,0 +1,20 @@
+from Map.ColliderType import *
+from Helpers.Artist import *
+
+class Tile():
+    x = 0
+    y = 0
+    imagePath = " "
+    cType = ColliderType
+
+
+    def __init__(self, x, y, imagePath):
+        self.x = x
+        self.y = y
+        self.imagePath = imagePath
+
+    def draw(self):
+        drawTextures(self.imagePath, self.x, self.y, 64, 64)
+
+    def setcType(self, cType):
+        self.cType = cType
